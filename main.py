97b827f7882795f7bcd7d4ced1c2de02084db99d
Ranwiesiel <ronggofc@gmail.com>
2024-12-14 17:49:52 +0700

    feat(klasifikasi citra): menambahkan fitur untuk mengklasifikasikan citra

--- main.py
+++ main.py
@@ -1,15 +1,74 @@
-from flask import Flask, redirect, url_for, render_template
-from programs.map import generate_map
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
+from programs.map import *
+from datetime import datetime
+import os
+import numpy as np
+from tensorflow.keras.models import load_model
+from PIL import Image
 
 app = Flask(__name__)
 
-@app.route('/')
+
+model = load_model('data/model/cnnModel.keras')
+
+# Tentukan ukuran gambar yang dibutuhkan oleh model
+img_width, img_height = 256, 256
+
+# Tentukan direktori penyimpanan gambar yang diupload
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    generate_map()
-    return render_template('map.html')
+    sby_timeseries, sby_temp, bkl_timeseries, bkl_temp = get_forecast_data()
+    sby_data, bkl_data = get_combined_data()
+    
+    filename = ''
+    predicted_class = ''
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        
+        if file:
+            # Simpan gambar yang diupload
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
+            
+            # Proses gambar untuk prediksi
+            img = Image.open(file_path).convert('RGB')
+            img = img.resize((img_width, img_height))
+            img_array = np.array(img) / 255.0  # Normalisasi gambar
+            img_array = np.expand_dims(img_array, axis=0)  # Tambahkan dimensi batch
+            
+            prediction = model.predict(img_array)
+            class_idx = np.argmax(prediction, axis=1)[0]
 
+            class_names = ['Berawan', 'Berkabut', 'Hujan', 'Cerah', 'Sunrise']
+            predicted_class = class_names[class_idx]
+        
+    return render_template('index.html',
+                        h_now=datetime.now().strftime('%H'),
+                        d_now=datetime.now().strftime('%d'),
+                        dname_now=datetime.now().strftime('%A'),
 
-    # return redirect(url_for('map'))
+                        sby_timeseries=sby_timeseries,
+                        sby_temp=sby_temp,
+                        bkl_timeseries=bkl_timeseries,
+                        bkl_temp=bkl_temp,
+
+                        sby_data=sby_data,
+                        bkl_data=bkl_data,
+                        
+                        filename=filename, predicted_class=predicted_class
+                        )
 
 # @app.route('/map')
 # def map():
--- programs/load_data.py
+++ programs/load_data.py
@@ -17,7 +17,45 @@ def get_data(data):
 def get_data_all(data):
     df = pd.read_csv(data, parse_dates=[0])
     df.rename(columns={df.columns[0]: 'date'}, inplace=True)
-    df['date'] = df['date'].dt.strftime("%Y-%m-%d jam %H")
-    timeseries = df.date.values
-    temp = df.predicted_mean.values
-    return timeseries, temp
+    df['years'] = df['date'].dt.strftime("%Y")
+    df['months'] = df['date'].dt.strftime("%m")
+    df['days'] = df['date'].dt.strftime("%A")
+    df['daysn'] = df['date'].dt.strftime("%d")
+    df['hours'] = df['date'].dt.strftime("%H").astype(int)
+    df['temp'] = df.predicted_mean.values.astype(int)
+    df = df[df['hours'] % 3 == 0]
+    return df.drop(columns=['predicted_mean'])
+
+def to_dict(df):
+	data_dict = {}
+
+	# Loop through years
+	for year in df['years'].unique():
+		data_dict[year] = {}
+		
+		# Filter data for the current year
+		year_data = df[df['years'] == year]
+		
+		# Loop through months
+		for month in year_data['months'].unique():
+			data_dict[year][month] = {}
+			
+			# Filter data for the current month
+			month_data = year_data[year_data['months'] == month]
+			
+			# Loop through unique dates
+			for date in month_data['daysn'].unique():
+				data_dict[year][month][date] = {}
+				
+				# Filter data for the current date
+				date_data = month_data[month_data['daysn'] == date]
+				# Get the name of the day
+				day_name = date_data['days'].values[0]
+				data_dict[year][month][date]['day_name'] = day_name
+				
+				# Loop through the hours and store the corresponding temperature
+				for hour in date_data['hours'].unique():
+					temp = date_data[date_data['hours'] == hour]['temp'].values[0]
+					data_dict[year][month][date][hour] = temp
+
+	return data_dict
